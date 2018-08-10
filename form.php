88fb869153b580a55de0b4a04ba3abffc44565a3
Marco García <marcogarcia.gon@gmail.com>
2018-08-10 12:50:31 -0500

    Primer intento AJAX
    Formulario de contacto

--- form.php
+++ form.php
@@ -1,10 +1,35 @@
 <?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST["name"];
+    $email= $_POST["email"];
+    $phone= $_POST["phone"];
+
+    $errorEmpty=false;
+    $errorEmail=false;
+
+if (empty($name) || empty($email)){
+    echo "<span class='form-error'>Por favor, llene todos los campos</span>";
+    $errorEmpty=true;
+}
+
+elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "<span class='form-error'>Ingrese un email válido</span>";
+    $errorEmail=true;
+}
+else{
+    echo "<span class='form-success'>Envío correcto</span>";
+}
+}
+else{
+    echo "<span class='form-error'>Ha ocurrido un error</span>";
+}
     $to = 'marcogarcia.gon@gmail.com';
     $name = $_POST["name"];
     $email= $_POST["email"];
     $phone= $_POST["phone"];
     
-
+/*
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
@@ -25,5 +50,22 @@
     }else{
         echo 'Error';
     }
-
+*/
 ?>
+
+<script type="text/javascript">
+$("#mail-name", "#mail-email", "#mail-phone").removeClass("input-error");
+
+    var errorEmpty="<?php echo $errorEmpty; ?>";
+    var errorEmail="<?php echo $errorEmail; ?>";
+
+    if (errorEmpty == true){
+        $("#mail-name", "#mail-email", "#mail-phone").addClass("input-error");
+    }
+    if (errorEmail ==true){
+         $("#mail-email").addClass("input-error");
+    }
+    if (errorEmpty==false &&errorMail ==false){
+        $("#mail-name", "#mail-email", "#mail-phone").val("");
+    }
+</script>
