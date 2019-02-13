66ede3c71f98867449338808f3bcc31ce7823a75
Marco García <marcogarcia.gon@gmail.com>
2019-02-12 18:54:59 -0600

    Cambio mail

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
-    $to = 'marcogarcia.gon@gmail.com';
+    $to = 'contacto@solucionesenefectivo.com';
     $name = $_POST["name"];
     $email= $_POST["email"];
     $text= $_POST["message"];
